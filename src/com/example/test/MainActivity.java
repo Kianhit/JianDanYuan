a8204608c3606334774943120fa480d84e6f1296
Kian <wylunaer@163.com>
2015-08-24 07:27:44 +0800

    left menu

--- src/com/example/test/MainActivity.java
+++ src/com/example/test/MainActivity.java
@@ -3,59 +3,26 @@ package com.example.test;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.util.DisplayMetrics;
-import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.Window;
 
-import com.astuetz.PagerSlidingTabStrip;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
-public class MainActivity extends FragmentActivity {
-	/**
-	 * 电影界面的Fragment
-	 */
-	private MovieFragment chatFragment;
-
-	/**
-	 * 说说界面的Fragment
-	 */
-	private ShuoshuoFragment foundFragment;
-
-	/**
-	 * 记事界面的Fragment
-	 */
-	private NoteFragment contactsFragment;
-
-	/**
-	 * PagerSlidingTabStrip的实例
-	 */
-	private PagerSlidingTabStrip tabs;
-
-	/**
-	 * 获取当前屏幕的密度
-	 */
-	private DisplayMetrics dm;
+public class MainActivity extends SlidingFragmentActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		setOverflowShowingAlways();
-		dm = getResources().getDisplayMetrics();
-		ViewPager pager = (ViewPager) findViewById(R.id.pager);
-		tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
-		pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
-		tabs.setViewPager(pager);
-		setTabsValue();
+		// 初始化SlideMenu
+		initLeftMenu();
 	}
 
 	@Override
@@ -101,71 +68,25 @@ public class MainActivity extends FragmentActivity {
 		}
 	}
 
-	/**
-	 * 对PagerSlidingTabStrip的各项属性进行赋值。
-	 */
-	private void setTabsValue() {
-		// 设置Tab是自动填充满屏幕的
-		tabs.setShouldExpand(true);
-		// 设置Tab的分割线是透明的
-		tabs.setDividerColor(Color.TRANSPARENT);
-		// 设置Tab底部线的高度
-		tabs.setUnderlineHeight((int) TypedValue.applyDimension(
-				TypedValue.COMPLEX_UNIT_DIP, 1, dm));
-		// 设置Tab Indicator的高度
-		tabs.setIndicatorHeight((int) TypedValue.applyDimension(
-				TypedValue.COMPLEX_UNIT_DIP, 4, dm));
-		// 设置Tab标题文字的大小
-		tabs.setTextSize((int) TypedValue.applyDimension(
-				TypedValue.COMPLEX_UNIT_SP, 16, dm));
-		// 设置Tab Indicator的颜色
-		tabs.setIndicatorColor(Color.parseColor("#45c01a"));
-		// 设置选中Tab文字的颜色 (这是我自定义的一个方法)
-		tabs.setSelectedTextColor(Color.parseColor("#45c01a"));
-		// 取消点击Tab时的背景色
-		tabs.setTabBackground(0);
+	private void initLeftMenu() {
+		Fragment leftMenuFragment = new MenuLeftFragment();
+		setBehindContentView(R.layout.left_menu_frame);
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.id_left_menu_frame, leftMenuFragment).commit();
+		SlidingMenu menu = getSlidingMenu();
+		menu.setMode(SlidingMenu.LEFT);
+		// 设置触摸屏幕的模式
+		menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+		menu.setShadowWidthRes(R.dimen.shadow_width);
+		menu.setShadowDrawable(R.drawable.shadow);
+		// 设置滑动菜单视图的宽度
+		menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+		// 设置渐入渐出效果的值
+		menu.setFadeDegree(0.35f);
+		menu.setSecondaryShadowDrawable(R.drawable.shadow);
 	}
 
-	public class MyPagerAdapter extends FragmentPagerAdapter {
-
-		public MyPagerAdapter(FragmentManager fm) {
-			super(fm);
-		}
-
-		private final String[] titles = { "电影", "说说", "记事" };
-
-		@Override
-		public CharSequence getPageTitle(int position) {
-			return titles[position];
-		}
-
-		@Override
-		public int getCount() {
-			return titles.length;
-		}
-
-		@Override
-		public Fragment getItem(int position) {
-			switch (position) {
-			case 0:
-				if (chatFragment == null) {
-					chatFragment = new MovieFragment();
-				}
-				return chatFragment;
-			case 1:
-				if (foundFragment == null) {
-					foundFragment = new ShuoshuoFragment();
-				}
-				return foundFragment;
-			case 2:
-				if (contactsFragment == null) {
-					contactsFragment = new NoteFragment();
-				}
-				return contactsFragment;
-			default:
-				return null;
-			}
-		}
-
+	public void showLeftMenu(View view) {
+		getSlidingMenu().showMenu();
 	}
 }
